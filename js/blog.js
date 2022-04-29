bc3ad431fa7164bc11d0984f9dd650ba2114ed67
pretzL <ericpretzinger@gmail.com>
2022-04-29 14:33:24 +0200

    added initial fetches for index, blog details and styled the outputs

---
+++ js/blog.js
@@ -0,0 +1,62 @@
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+// if the id is null, then redirect to the home page
+if (id === null) {
+  location.href = "/";
+}
+
+const baseURL = "http://pretzl.one/foodforthought/wp-json/wp/v2/posts/";
+
+const detailsURL = baseURL + id + "?_embed";
+
+const blogContent = document.querySelector(".blog-page-content");
+
+const pageTitle = document.querySelector("title");
+const pageHeading = document.querySelector(".page-heading");
+
+const blogBanner = document.querySelector(".blog-page-banner");
+
+async function fetchSingleRecipe() {
+  try {
+    const response = await fetch(detailsURL);
+    const result = await response.json();
+
+    console.log(result);
+
+    pageTitle.innerText = result.title.rendered + " | Food For Thought";
+    pageHeading.innerText = result.title.rendered;
+    blogBanner.innerHTML = `<img src="${result._embedded["wp:featuredmedia"]["0"].source_url}" alt="${result.title.rendered}" class="blog-page-banner-image" />`;
+
+    blogContent.innerHTML = `
+    <div class="blog-page-card blog-description blog-grid1">
+        <h3>Description<h3>
+        <p>${result.acf.description}</p>
+    </div>
+    <div class="blog-page-card blog-recipe blog-grid2">
+        <h3>Description<h3>
+        <p>${result.acf.recipe}</p>
+    </div>
+    <div class="blog-page-card blog-about blog-grid3">
+        <h3>About<h3>
+        <div class="blog-author-info">
+            <img src="${result._embedded.author[0].avatar_urls[96]}" alt="${result._embedded.author[0].name} avatar image" class="user-profile-image"/>
+            <div>
+            <h4 class="author-name-title">${result._embedded.author[0].name}</h4>
+            <p>${result._embedded.author[0].description}</p>
+            </div>
+        </div>
+    </div>
+    <div class="blog-page-card blog-similar blog-grid4">
+        <h3>Similar Posts<h3>
+    </div>`;
+  } catch (error) {
+    console.log(error);
+    errorContainer.innerHTML = errorMessage("An error occurred when calling the API, error: " + error);
+  }
+}
+
+fetchSingleRecipe();
---
+++ js/index.js
@@ -0,0 +1,36 @@
+const carouselContainer = document.querySelector(".carousel-container");
+
+// FETCH
+
+const baseURL = "http://pretzl.one/foodforthought/wp-json/wp/v2/posts?_embed";
+
+async function getRecipes() {
+  try {
+    const response = await fetch(baseURL);
+    const results = await response.json();
+
+    console.log(results);
+
+    carouselContainer.innerHTML = "";
+
+    for (let i = 0; i < results.length; i++) {
+      const date = results[i].date;
+      const dateFix = date.split("T")[0];
+
+      console.log(dateFix);
+
+      carouselContainer.innerHTML += `<a href="/blog.html?id=${results[i].id}" class="card">
+        <img src="${results[i]._embedded["wp:featuredmedia"]["0"].source_url}" class="card-image" alt="${results[i].title.rendered}"/>
+        <h3>${results[i].title.rendered}</h3>
+        <p>Posted: ${dateFix}</p>
+        </a>`;
+    }
+  } catch (error) {
+    console.log(error);
+    errorContainer.innerHTML = errorMessage("An error occurred when calling the API, error: " + error);
+  }
+}
+
+getRecipes();
+
+// CAROUSEL
