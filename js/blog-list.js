b3941bdc912c1c90cba05570dd9eb5061b724384
pretzL <ericpretzinger@gmail.com>
2022-04-29 18:04:32 +0200

    added blog-list fetch

---
+++ js/blog-list.js
@@ -0,0 +1,29 @@
+const blogList = document.querySelector(".blog-list-container");
+
+// FETCH
+
+const baseURL = "http://pretzl.one/foodforthought/wp-json/wp/v2/posts?_embed";
+
+async function getRecipes() {
+  try {
+    const response = await fetch(baseURL);
+    const results = await response.json();
+
+    console.log(results);
+
+    blogList.innerHTML = "";
+
+    for (let i = 0; i < results.length; i++) {
+      blogList.innerHTML += `<a href="/blog.html?id=${results[i].id}" class="card">
+        <img src="${results[i]._embedded["wp:featuredmedia"]["0"].source_url}" class="card-image" alt="${results[i].title.rendered}"/>
+        <h3>${results[i].title.rendered}</h3>
+        <p>Posted: ${results[i].x_date}</p>
+        </a>`;
+    }
+  } catch (error) {
+    console.log(error);
+    errorContainer.innerHTML = errorMessage("An error occurred when calling the API, error: " + error);
+  }
+}
+
+getRecipes();
--- js/index.js
+++ js/index.js
@@ -18,11 +18,6 @@ async function getRecipes() {
     carouselContainer.innerHTML = "";
 
     for (let i = 0; i < results.length; i++) {
-      const date = results[i].date;
-      const dateFix = date.split("T")[0];
-
-      console.log(dateFix);
-
       carouselContainer.innerHTML += `<a href="/blog.html?id=${results[i].id}" class="card">
         <img src="${results[i]._embedded["wp:featuredmedia"]["0"].source_url}" class="card-image" alt="${results[i].title.rendered}"/>
         <h3>${results[i].title.rendered}</h3>
