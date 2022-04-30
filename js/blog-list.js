b027b028edbfa8825ecdbba1ada6a8b71cacd3a2
pretzL <ericpretzinger@gmail.com>
2022-04-30 17:25:50 +0200

    blog-list grid

--- js/blog-list.js
+++ js/blog-list.js
@@ -14,10 +14,15 @@ async function getRecipes() {
     blogList.innerHTML = "";
 
     for (let i = 0; i < results.length; i++) {
-      blogList.innerHTML += `<a href="/blog.html?id=${results[i].id}" class="card">
+      const date = results[i].date;
+      const dateFix = date.split("T")[0];
+
+      const gridClass = "blog-list-grid" + (i + 1);
+
+      blogList.innerHTML += `<a href="/blog.html?id=${results[i].id}" class="card blog-card ${gridClass}">
         <img src="${results[i]._embedded["wp:featuredmedia"]["0"].source_url}" class="card-image" alt="${results[i].title.rendered}"/>
         <h3>${results[i].title.rendered}</h3>
-        <p>Posted: ${results[i].x_date}</p>
+        <p>Posted: ${dateFix}</p>
         </a>`;
     }
   } catch (error) {
