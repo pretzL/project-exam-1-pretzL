cd774f4b84b8c5d1cd3c5f6e76c2bdb87e6f2e5e
pretzL <ericpretzinger@gmail.com>
2022-05-06 12:47:40 +0200

    removed image fetch from search page atleast for the time being

--- js/search-page.js
+++ js/search-page.js
@@ -34,7 +34,6 @@ async function getSearch() {
       const dateFix = date.split("T")[0];
 
       searchContainer.innerHTML += `<a href="/blog.html?id=${results[i]._embedded.self[0].id}" class="card">
-      <img src="${results[i]._embedded.self[0].acf.banner_image}" class="card-image" alt="${results[i]._embedded.self[0].title.rendered}"/>
       <h3>${results[i]._embedded.self[0].title.rendered}</h3>
       <p>Posted: ${dateFix}</p>
       </a>`;
